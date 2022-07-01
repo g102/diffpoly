0894a2254aa016a6b97f24c70ed25cc0e6c3e2bd
Stefano <32854170+g102@users.noreply.github.com>
2022-07-01 13:55:07 +0000

    Update diffpoly.m
    Changed the function to take as input "w" the total width of the stencil, not the half-width. "w" must now be odd.

--- diffpoly.m
+++ diffpoly.m
@@ -1,8 +1,7 @@
 function dy = diffpoly(x, y, w, p)
 % dy = DIFFPOLY(x, y, w, p)
 %   Compute the derivative of y(x) by computing the derivative of the the
-%   fitting polynomial of degree POLYDEG on a stencil of half-width w
-%   (total width 2*w + 1)
+%   fitting polynomial of degree p on a stencil of width w
 %   At the boundaries of the domain, the stencil has always the same size
 %   but it is not centered
 %
@@ -12,7 +11,7 @@ function dy = diffpoly(x, y, w, p)
 %   Inputs:
 %     x: points where the function y(x) has been computed
 %     y: values of the function y(x) in the points specified in 'x'
-%     w: half-width of the stencil to be used to compute the polynomial
+%     w: width of the stencil used to compute the polynomial (must be odd)
 %     p: degree of the interpolating polynomial
 %   All the input values are mandatory, none are optional
 %
@@ -20,7 +19,7 @@ function dy = diffpoly(x, y, w, p)
 %     dy: estimated derivative y'(x) computed in the points in 'x'
 %   
 %   Limitations:
-%     the stencil is assumed to be symmetric (from -w to +w)
+%     the stencil is assumed to be symmetric around each point
 %     x and y must be vectors, with x increasing
 %     y has no NaNs
 %
@@ -29,8 +28,8 @@ function dy = diffpoly(x, y, w, p)
 
 % Algorithm:
 %   for x0 in x:
-%       define a neighbourhood of x0 of radius w
-%       if the distance of x0 from the boundary is less than w:
+%       define a neighbourhood of x0 of radius hw
+%       if the distance of x0 from the boundary is less than hw:
 %           make the neighb. not centered around x0 but keep width = w
 %       compute the best fitting polynomial of degree p in the neighb.
 %       estimate dydx(x0) as the derivative of the poynomial computed
@@ -38,9 +37,17 @@ function dy = diffpoly(x, y, w, p)
 %% check on arguments
 narginchk(4, 4);
 
+% errors
 if ~isvector(x) || ~isvector(y)
 	error('diffpoly only works on vectors');
 end
+if length(x) ~= length(y)
+	error('x and y must have the same number of elements');
+if ~mod(w, 2)
+	error('Stencil width must be odd');
+end
+
+% warnings
 if floor(w) ~= w
 	w = floor(w);
 	warning('Stencil width is not integer, rounded down to %d', w);
@@ -57,9 +64,9 @@ end
 if any(diff(x) < 0)
 	error('x must be an increasing vector of points');
 end
-if p > 2*w + 1
+if p > w
 	error('P is higher than the number of points in the stencil');
-elseif p == 2*w + 1
+elseif p == w
 	warning('P is equal to the number of points in the stencil');
 end
 if ~iscolumn(y)
@@ -85,6 +92,7 @@ end
 
 % shorthand
 Nx = length(x);
+hw = (w-1)/2;
 
 % build single large Vandermonde matrix
 V = ones(Nx, p+1);
@@ -94,31 +102,31 @@ for jj = 2 : p+1
 end
 
 %% compute dy
-% since the interpolating polynomial is the same in the first 'w' points,
+% since the interpolating polynomial is the same in the first 'hw' points,
 % only compute it once, then reuse it for all points with the same stencil
 bLeftBoundComputed = false;
 bRightBoundComputed = false;
 for jj = 1:length(y)
-	if jj <= w
+	if jj <= hw
 		% close to the left boundary
 		i1 = 1;
-		i2 = 2*w + 1;
+		i2 = 2*hw + 1;
 		if ~bLeftBoundComputed
 			dp = polyder(flip(transpose(V(i1:i2, :)\y(i1:i2))));
 			bLeftBoundComputed = true;
 		end
-	elseif jj >= Nx - w
+	elseif jj >= Nx - hw
 		% close to the right boundary
 		i2 = Nx;
-		i1 = i2 - 2*w + 1;
+		i1 = i2 - 2*hw + 1;
 		if ~bRightBoundComputed
 			dp = polyder(flip(transpose(V(i1:i2, :)\y(i1:i2))));
 			bLeftBoundComputed = true;
 		end
 	else
 		% in the middle
-		i1 = jj - w;
-		i2 = jj + w;
+		i1 = jj - hw;
+		i2 = jj + hw;
 		dp = polyder(flip(transpose(V(i1:i2, :)\y(i1:i2))));
 	end
 	dy(jj) = polyval(dp, x(jj));
